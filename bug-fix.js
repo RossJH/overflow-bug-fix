b8c32610292462ab9f98b778c2ecc06fc99fa966
Ross Hardingham <30514204+RossJH@users.noreply.github.com>
2021-10-28 19:41:59 +1100

    Overflow JS to find bug

---
+++ bug-fix.js
@@ -0,0 +1,12 @@
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+    document.querySelectorAll('*'),
+    function (el) {
+        if (el.offsetWidth > docWidth) {
+            console.log(el);
+        }
+    }
+);
+
+//Add this to console and hit enter it will usually show what element is causing overflow. 
